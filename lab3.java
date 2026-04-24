907b59bdd785a96284e9b4e862ff3e2bb3c90aa7
p1an0guy <123jonahchan@gmail.com>
2026-04-24 11:48:21 -0700

    feat: partial support for interactive mode

--- lab3.java
+++ lab3.java
@@ -1,6 +1,7 @@
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner; // Import the Scanner class to read text files
 
@@ -8,7 +9,7 @@ public class lab3 {
 
     static HashMap<String, Integer> labelAddr = new HashMap<>(); // hash map to store labels + their addr
     static ArrayList<Instruction> instructionArray = new ArrayList<>();
-    static int[] registers = new int[32];
+    static int[] registers = new int[32]; // N.B. remember to always set $0 and $zero to 0!!
     static int[] dataMem = new int[8192];
     static int PC;
 
@@ -226,18 +227,113 @@ public class lab3 {
 
     public static void interactiveMode() {
         Scanner usrInput = new Scanner(System.in);
-        System.out.print("mips> ");
-        String input = usrInput.next();
-        executeCmd(input);
+        while (true) {
+            System.out.print("mips> ");
+            String input = usrInput.next();
+            if (input.charAt(0) == 'q') break;
+            executeCmd(input);
+        }
 
         usrInput.close();
     }
 
     public static void executeCmd(String cmd) {
         switch (cmd.charAt(0)) {
-            case 'h': // show help
+            case 'h': // sh = show help
+                System.out.println("d = dump register state");
+                System.out.println(
+                    "s = single step through the program (i.e. execute 1 instruction and stop)"
+                );
+                System.out.println(
+                    "s num = step through num instructions of the program"
+                );
+                System.out.println("r = run until the program ends");
+                System.out.println(
+                    "m num1 num2 = display data memory from location num1 to num2"
+                );
+                System.out.println(
+                    "c = clear all registers, memory, and the program counter to 0"
+                );
+                System.out.println("q = exit the program");
                 break;
             case 'd': // dump register state
+                System.out.println("\npc = " + PC);
+                // $0 = 0          $v0 = 0         $v1 = 0         $a0 = 0
+                // $a1 = 0         $a2 = 0         $a3 = 0         $t0 = 0
+                // $t1 = 0         $t2 = 0         $t3 = 0         $t4 = 0
+                // $t5 = 0         $t6 = 0         $t7 = 0         $s0 = 0
+                // $s1 = 0         $s2 = 0         $s3 = 0         $s4 = 0
+                // $s5 = 0         $s6 = 0         $s7 = 0         $t8 = 0
+                // $t9 = 0         $sp = 0         $ra = 0
+                System.out.println(
+                    "$0 = " +
+                        registers[0] +
+                        "\t$v0 = " +
+                        registers[2] +
+                        "\t$v1 = " +
+                        registers[3] +
+                        "\t$a0 = " +
+                        registers[4]
+                );
+                System.out.println(
+                    "$a1 = " +
+                        registers[5] +
+                        "\t$a2 = " +
+                        registers[6] +
+                        "\t$a3 = " +
+                        registers[7] +
+                        "\t$t0 = " +
+                        registers[8]
+                );
+                System.out.println(
+                    "$t1 = " +
+                        registers[9] +
+                        "\t$t2 = " +
+                        registers[10] +
+                        "\t$t3 = " +
+                        registers[11] +
+                        "\t$t4 = " +
+                        registers[12]
+                );
+                System.out.println(
+                    "$t5 = " +
+                        registers[13] +
+                        "\t$t6 = " +
+                        registers[14] +
+                        "\t$t7 = " +
+                        registers[15] +
+                        "\t$s0 = " +
+                        registers[16]
+                );
+                System.out.println(
+                    "$s1 = " +
+                        registers[17] +
+                        "\t$s2 = " +
+                        registers[18] +
+                        "\t$s3 = " +
+                        registers[19] +
+                        "\t$s4 = " +
+                        registers[20]
+                );
+                System.out.println(
+                    "$s5 = " +
+                        registers[21] +
+                        "\t$s6 = " +
+                        registers[22] +
+                        "\t$s7 = " +
+                        registers[23] +
+                        "\t$t8 = " +
+                        registers[24]
+                );
+                System.out.println(
+                    "$t9 = " +
+                        registers[25] +
+                        "\t$sp = " +
+                        registers[29] +
+                        "\t$ra = " +
+                        registers[31]
+                );
+
                 break;
             case 's': // single step through program
                 // see if there are more args
@@ -247,8 +343,10 @@ public class lab3 {
             case 'm': // m n1 n2 display dataMem from n1 to n2
                 break;
             case 'c': // clear all registers, mem, reset PC
-                break;
-            case 'q': // exit program
+                Arrays.fill(registers, 0);
+                Arrays.fill(dataMem, 0);
+                PC = 0;
+                System.out.println("\tSimulator reset");
                 break;
             default:
                 System.out.println("Error: Invalid command. 'h' for help menu");
